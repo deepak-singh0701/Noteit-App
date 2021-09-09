e2daf565b412d166ea4afaea65747ab182530d29
deepak-singh0701 <deepak0701singh@gmail.com>
2021-09-09 13:47:51 +0530

    fix: npm build error

---
+++ build/precache-manifest.8a87c30b52da475f2bc3c677f87b19d0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6ba7d83d074128231cb77e63801171",
+    "url": "/deepak-singh0701/Noteit-App/index.html"
+  },
+  {
+    "revision": "9adc41c60126eac7aa79",
+    "url": "/deepak-singh0701/Noteit-App/static/js/2.9d3d7f2b.chunk.js"
+  },
+  {
+    "revision": "d52ee095770df9d9269c",
+    "url": "/deepak-singh0701/Noteit-App/static/js/main.faee637a.chunk.js"
+  },
+  {
+    "revision": "97de5310408a710a34a2",
+    "url": "/deepak-singh0701/Noteit-App/static/js/runtime-main.8b0e3444.js"
+  }
+]);
